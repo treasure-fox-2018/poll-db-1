b98e9b7c5617aadd7a5674ded4362d13190c7ad5
Rhesa Utomo <venderblitz@gmail.com>
2018-06-25 16:16:50 +0700

    release 3 fixed

--- setup.js
+++ setup.js
@@ -1,37 +1,26 @@
 const db = require('./db')
-const fs = require('fs')
 
-const politicians = fs.readFileSync('./politicians.csv', 'utf8').split('\n')
-for (let i = 1; i < politicians.length; i++) {
-  let politician = politicians[i].split(',')
-  let query = `INSERT INTO politicians (name, party, location, grade_current)
-               VALUES ('${politician[0]}', '${politician[1]}', '${politician[2]}', '${politician[3]}')`
-  db.run(query, function(err) {
-    if (err) throw err
-    console.log(`Inserted politician data ${i}`)
-  })
-}
+db.serialize(function() {
+  db.get("PRAGMA foreign_keys = ON")
+  
+  db.run(`CREATE TABLE IF NOT EXISTS "politicians"  (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          name VARCHAR UNIQUE,
+          party VARCHAR,
+          location VARCHAR,
+          grade_current	REAL)`)
 
-const voters = fs.readFileSync('./voters.csv', 'utf8').split('\n')
-for (let i = 1; i < voters.length; i++) {
-  let columnName = voters[0]
-  let voter = voters[i].split(',')
-  let query = `INSERT INTO voters (${columnName})
-               VALUES ('${voter[0]}', "${voter[1]}", '${voter[2]}', '${voter[3]}')`
-  db.run(query, function(err) {
-    if (err) throw err
-    console.log(`Inserted voter data ${i}`)
-  })
-}
+  db.run(`CREATE TABLE IF NOT EXISTS "voters" (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          first_name VARCHAR,
+          last_name VARCHAR,
+          gender VARCHAR,
+          age INTEGER)`)
 
-const votes = fs.readFileSync('./votes.csv', 'utf8').split('\n')
-for (let i = 1; i < votes.length; i++) {
-  let columnName = votes[0]
-  let vote = votes[i].split(',')
-  let query = `INSERT INTO votes (${columnName})
-               VALUES (${vote[0]}, ${vote[1]})`
-  db.run(query, function(err) {
-    if (err) throw err
-    console.log(`Inserted vote data ${i}`)
-  })
-}
+  db.run(`CREATE TABLE IF NOT EXISTS "votes" (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          voterId INTEGER,
+          politicianId INTEGER,
+          FOREIGN KEY (politicianId) REFERENCES politicians(id),
+          FOREIGN KEY (voterId) REFERENCES voters(id))`)
+})
