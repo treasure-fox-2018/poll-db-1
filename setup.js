2ad79af21117c4fe6592fb51ed2637849a57e600
vinesto <oqvinesto@gmail.com>
2018-06-25 17:34:07 +0700

    done

--- setup.js
+++ setup.js
@@ -1,1 +1,36 @@
 //your code here
+"use strict"
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./database_poll');
+
+function createTable(){
+    db.serialize(function(){
+    
+        db.run(`CREATE TABLE IF NOT EXISTS politicians(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            party TEXT,
+            location TEXT,
+            grade_current REAL
+        )`);
+    
+        db.run(`CREATE TABLE IF NOT EXISTS voters(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            first_name TEXT,
+            last_name TEXT,
+            gender TEXT,
+            age INTEGER
+        )`);
+        
+        db.run(`CREATE TABLE IF NOT EXISTS votes(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            voterID INTEGER,
+            politicianID INTEGER
+        )`);
+    
+         
+    })
+}
+
+createTable();
+
